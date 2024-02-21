470b94a73cd2da84dd72e1fc3d0c2f9f8c48a6fb
akir0 <kirill.andreevich.dev@gmail.com>
2024-02-21 23:00:26 +0300

    feat: added simple counter component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import Counter from './components/Counter'
 
 function App() {
 
   return (
     <>
       <div>
-        
+        <Counter></Counter>
       </div>
     </>
   )
