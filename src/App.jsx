9797259707ca6effe4609e0a733076a0ce14a026
akir0 <kirill.andreevich.dev@gmail.com>
2024-02-21 23:59:22 +0300

    feat: posts array and some fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
 import './styles/App.css'
 import Counter from './components/Counter'
 import PostItem from './components/PostItem'
+import PostList from './components/PostList'
+import { useState } from 'react'
 
 function App() {
-
+const [posts, setPosts] = useState([
+  {id:1, title:'Post 1', body:'Description'},
+  {id:2, title:'Post 2', body:'Description 2'},
+  {id:3, title:'Post 3', body:'Description 3'},
+])
   return (
     <>
-      <div>
-        <Counter></Counter>
-        <PostItem post={{id:1, title: 'Javascript', body: 'Description'}}/>
+      <div className='App'>
+    <Counter/>
+    <PostItem post={{id:1, title:'Javascript', body: 'Description'}}/>
+    <PostList posts={posts} title='Post List 1'/>
+    
+    
       </div>
     </>
   )
