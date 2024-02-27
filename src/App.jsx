1ab07468275b8b870b5c21c229b46a3a46c95056
akir0 <kirill.andreevich.dev@gmail.com>
2024-02-27 05:28:23 +0300

    feat: decomposition. custom hook useFetching

--- src/App.jsx
+++ src/App.jsx
@@ -9,11 +9,12 @@ import PostForm from "./components/PostForm";
 import MySelect from "./components/UI/select/MySelect";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
-import {CSSTransition, TransitionGroup} from "react-transition-group"
+import { CSSTransition, TransitionGroup } from "react-transition-group";
 import { usePosts } from "./hooks/usePosts";
-import axios from 'axios'
+import axios from "axios";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
+import { useFetching } from "./hooks/useFetching";
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -21,26 +22,19 @@ function App() {
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
-  const [isPostsLoading, setIsPostsLoading] = useState(false);
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+    const posts = await PostService.getAll();
+    setPosts(posts);
+  });
 
-  useEffect( () => {
-fetchPosts()
-  }, [] )
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
-
-async function fetchPosts() {
-  setIsPostsLoading(true);
-  setTimeout(async () => {
-    const posts = await PostService.getAll();
-    setPosts(posts)
-    setIsPostsLoading(false);
-  }, 1000)
-
-}
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -50,23 +44,28 @@ async function fetchPosts() {
     <>
       <div className="App">
         <Counter />
-        <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Create user post</MyButton>
+        <MyButton style={{ marginTop: 30 }} onClick={() => setModal(true)}>
+          Create user post
+        </MyButton>
         <MyModal visible={modal} setVisible={setModal}>
-        <PostForm create={createPost} />
+          <PostForm create={createPost} />
         </MyModal>
         <hr style={{ margin: "15px 0" }} />
         <PostFilter filter={filter} setFilter={setFilter} />
-        {isPostsLoading
-        ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}>
-          <Loader/>
-        </div>
-        :  <PostList
-        remove={removePost}
-        posts={sortedAndSearchedPosts}
-        title="Post List 1"
-      />
-        }
-       
+        {postError && <h1>Error</h1>}
+        {isPostsLoading ? (
+          <div
+            style={{ display: "flex", justifyContent: "center", marginTop: 50 }}
+          >
+            <Loader />
+          </div>
+        ) : (
+          <PostList
+            remove={removePost}
+            posts={sortedAndSearchedPosts}
+            title="Post List 1"
+          />
+        )}
       </div>
     </>
   );
