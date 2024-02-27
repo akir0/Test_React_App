2dc6cbdc8e918d44d8cd05ab8c97d8359df7f714
akir0 <kirill.andreevich.dev@gmail.com>
2024-02-27 04:33:01 +0300

    feat: random posts from jsonplaceholder. useEffect usage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import "./styles/App.css";
 import Counter from "./components/Counter";
 import PostItem from "./components/PostItem";
 import PostList from "./components/PostList";
-import { useMemo, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 import MyButton from "./components/UI/button/MyButton";
 import MyInput from "./components/UI/input/MyInput";
 import PostForm from "./components/PostForm";
@@ -11,22 +11,28 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import {CSSTransition, TransitionGroup} from "react-transition-group"
 import { usePosts } from "./hooks/usePosts";
+import axios from 'axios'
 
 function App() {
-  const [posts, setPosts] = useState([
-    { id: 1, title: "Post 2a", body: "Description 1e" },
-    { id: 2, title: "Post 1b", body: "Description 2f" },
-    { id: 3, title: "Post 3c", body: "Description 3g" },
-  ]);
+  const [posts, setPosts] = useState([]);
 
   const [filter, setFilter] = useState({ sort: "", query: "" });
   const [modal, setModal] = useState(false);
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
 
+  useEffect( () => {
+fetchPosts()
+  }, [] )
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
     setModal(false);
   };
+
+async function fetchPosts() {
+const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+setPosts(response.data)
+}
 
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id));
@@ -36,7 +42,6 @@ function App() {
     <>
       <div className="App">
         <Counter />
-        <PostItem post={{ id: 1, title: "Javascript", body: "Description" }} />
         <MyButton style={{marginTop: 30}} onClick={() => setModal(true)}>Create user post</MyButton>
         <MyModal visible={modal} setVisible={setModal}>
         <PostForm create={createPost} />
