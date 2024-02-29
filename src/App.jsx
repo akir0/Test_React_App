7fa8135e158c468cd785f828f3eef042b5569d08
akir0 <kirill.andreevich.dev@gmail.com>
2024-03-01 00:38:25 +0300

    feat: added dynamic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -13,16 +13,18 @@ import Posts from "./pages/Posts";
 import Navbar from "./components/UI/Navbar/navbar";
 import CounterPage from "./pages/CounterPage";
 import Error from "./pages/Error";
+import PostIdPage from "./pages/PostIdPage";
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
         <Route path="/about" element={<About />}></Route>
-        <Route path="/posts" element={<Posts />}></Route>
+        <Route exact path="/posts" element={<Posts />}></Route>
         <Route path="/counter" element={<CounterPage />}></Route>
         <Route path="/error" element={<Error />}></Route>
         <Route path="*" element={<Navigate to="/error" replace />} />
+        <Route exact path="/posts/:id" element={<PostIdPage />}></Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import MyButton from "./UI/button/MyButton";
+import { useNavigate } from "react-router-dom";
 
 const PostItem = (props) => {
-    return (
-        <div className="post">
-            <div className="post__content">
-            <strong>{props.post.id}. {props.post.title}</strong>
-            <div>
-            {props.post.body}
-            </div>
-        </div>
-        <div className="post__btns">
-            <MyButton onClick={() => props.remove(props.post)} >Delete Post</MyButton>
-        </div>
-        </div>
-    );
+  const router = useNavigate();
+  console.log(router);
+  return (
+    <div className="post">
+      <div className="post__content">
+        <strong>
+          {props.post.id}. {props.post.title}
+        </strong>
+        <div className="">{props.post.body}</div>
+      </div>
+      <div className="post__btns">
+        <MyButton onClick={() => router(`/posts/${props.post.id}`)}>
+          Open Post
+        </MyButton>
+        <MyButton onClick={() => props.remove(props.post)}>
+          Delete Post
+        </MyButton>
+      </div>
+    </div>
+  );
 };
 
 export default PostItem;
