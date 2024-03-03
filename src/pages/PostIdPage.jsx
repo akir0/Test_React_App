08504b36a39b9f0f88648031d8290af978368701
akir0 <kirill.andreevich.dev@gmail.com>
2024-03-03 19:37:38 +0300

    feat: added comments

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -7,13 +7,20 @@ import Loader from "../components/UI/Loader/Loader";
 const PostIdPage = () => {
   const params = useParams();
   const [post, setPost] = useState({});
+  const [comments, setComments] = useState([]);
   const [fetchPostById, isLoading, error] = useFetching(async (id) => {
     const response = await PostService.getById(id);
     setPost(response.data);
   });
 
+  const [fetchComments, isComLoading, comError] = useFetching(async (id) => {
+    const response = await PostService.getCommentsById(id);
+    setComments(response.data);
+  });
+
   useEffect(() => {
     fetchPostById(params.id);
+    fetchComments(params.id);
   }, []);
 
   return (
@@ -26,6 +33,17 @@ const PostIdPage = () => {
           {post.id}. Title:{post.title}. Text:{post.body}
         </div>
       )}
+       <h1>Comments</h1>
+          {isComLoading
+          ? <Loader/>
+          : <div>
+            {comments.map(comm => 
+              <div style={{marginTop: 15}}>
+                <h5>{comm.email}</h5>
+                <div>{comm.body}</div>
+              </div>)}
+          </div>
+          }
     </div>
   );
 };
